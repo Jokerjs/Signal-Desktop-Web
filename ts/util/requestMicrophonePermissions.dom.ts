30979099ef6a33803471eaf08b43f36482fb6243
ahui <8131746272@qq.com>
2026-07-11 16:41:00 +0800

    User-Agent

--- ts/util/requestMicrophonePermissions.dom.ts
+++ ts/util/requestMicrophonePermissions.dom.ts
@@ -1,38 +1,9 @@
 // Copyright 2021 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
 
-function isSignalWebRuntime(): boolean {
-  const config = window.SignalContext?.config as unknown;
-  return (
-    config != null && typeof config === 'object' && 'renderApiBaseUrl' in config
-  );
-}
-
-async function requestBrowserMicrophonePermissions(): Promise<boolean> {
-  if (!navigator.mediaDevices?.getUserMedia) {
-    return false;
-  }
-
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: {
-        channelCount: { ideal: 1 },
-      },
-    });
-    stream.getTracks().forEach(track => track.stop());
-    return true;
-  } catch {
-    return false;
-  }
-}
-
 export async function requestMicrophonePermissions(
   forCalling: boolean
 ): Promise<boolean> {
-  if (isSignalWebRuntime()) {
-    return requestBrowserMicrophonePermissions();
-  }
-
   const microphonePermission = await window.IPC.getMediaPermissions();
   if (!microphonePermission) {
     await window.IPC.showPermissionsPopup(forCalling, false);
